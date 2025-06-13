bb0282aea8ef7f875b2a8b30c06ee7580887999a
aurelie <aurelie.audemar@orange.fr>
2025-06-13 11:33:47 +0200

    Premier commit

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        myCustomGlobal: "readonly"
+      }
+    }
+  }
+]);
